438aaedfa8c8569f27abc5ae7fab4300f688dcad
lrg1031@live.com <>
2021-10-15 15:04:41 -0400

    This is the picture GUI, has full functionality.

--- src/main/java/com/mycompany/luke_green_week7_lab/PrimaryController.java
+++ src/main/java/com/mycompany/luke_green_week7_lab/PrimaryController.java
@@ -1,19 +1,81 @@
 package com.mycompany.luke_green_week7_lab;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class PrimaryController {
     
+    //i have some code for a secondary view, but ended up only using t5he primary view to acheive the goal
+    
     int counter = 0;
+    int rotateCounter = 0;
+    
+    @FXML
+    Label myCounterLabel;
+    
+    @FXML
+    private ImageView fishPic;
+    
+    @FXML
+    private ImageView goosePic;
+
+    @FXML
+    private Label pictureLabel1;
+
+    @FXML
+    private Label pictureLabel2;
+
+    @FXML
+    private Button primaryButton;
+    
+    @FXML
+    private Button rotateButton;
 
     @FXML
     private void switchToSecondary() throws IOException {
+        counter++;
+        myCounterLabel.setText(""+counter);
         App.setRoot("secondary");
     }
     
+    //goose to fish
     @FXML
-    private char counter() {
-        return (char)counter;
+    private void switchPicture1() throws IOException {
+        counter++;
+        myCounterLabel.setText(""+counter);
+        goosePic.setOpacity(0);
+        fishPic.setOpacity(1);
+    }
+    
+    //fish to goose
+    @FXML
+    private void switchPicture2() throws IOException {
+        counter++;
+        myCounterLabel.setText(""+counter);
+        goosePic.setOpacity(1);
+        fishPic.setOpacity(0);
+    }
+    
+    //to rotate the picture
+    @FXML
+    private void rotatePicture() throws IOException {
+        if(rotateCounter == 0) {
+        counter++;
+        myCounterLabel.setText(""+counter);
+        rotateCounter++;
+        goosePic.setRotate(25);
+        fishPic.setRotate(25);
+        }
+        else {
+        counter++;
+        myCounterLabel.setText(""+counter);
+        rotateCounter--;
+        goosePic.setRotate(0);
+        fishPic.setRotate(0);
+        }
     }
 }
--- src/main/java/com/mycompany/luke_green_week7_lab/SecondaryController.java
+++ src/main/java/com/mycompany/luke_green_week7_lab/SecondaryController.java
@@ -2,11 +2,27 @@ package com.mycompany.luke_green_week7_lab;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class SecondaryController {
+    
+    @FXML
+    private ImageView fishPic;
+
+    @FXML
+    private Label myCounterLabel;
+
+    @FXML
+    private Button secondaryButton;
+    
+    @FXML
+    private Label pictureLabel;
 
     @FXML
     private void switchToPrimary() throws IOException {
+        
         App.setRoot("primary");
     }
 }
